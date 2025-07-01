e3b8600bdd0b783c640a9f8068adb8336bba7611
Willian Santos <168508258+Hyrat16@users.noreply.github.com>
2025-07-01 20:54:08 -0300

    Delete To-Do-List directory
    Remove To-Do excessivo

--- To-Do-List/src/components/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer({ listTask }: { listTask: [] }) {
-  return (
-    <footer className="todoFooter">
-      <p>
-        <span id="pendingTasksCount">{listTask.length}</span> tarefas pendentes
-      </p>
-    </footer>
-  );
-}
